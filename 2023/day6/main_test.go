3a4d9bf0a8252b9dce188d07238a1441c5b102aa
Damitha <damitha@canva.com>
2023-12-06 23:05:50 +1100

    adding asserts

--- 2023/day6/main_test.go
+++ 2023/day6/main_test.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var i = `Time:      7  15   30
@@ -12,18 +13,15 @@ func TestTask1(t *testing.T) {
 	got := task1(i)
 	want := 288
 
-	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
-	}
+	assert.Equal(t, got, want)
 }
 
 func TestTask2(t *testing.T) {
 
 	got := task2(i)
 	want := 71503
-	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
-	}
+
+	assert.Equal(t, got, want)
 }
 
 func TestParsing(t *testing.T) {
@@ -31,11 +29,6 @@ func TestParsing(t *testing.T) {
 	expectedTimes := []int{7, 15, 30}
 	expectedDistances := []int{9, 40, 200}
 
-	if !reflect.DeepEqual(times, expectedTimes) {
-		t.Errorf("got %d, wanted %d", times, expectedTimes)
-	}
-
-	if !reflect.DeepEqual(distances, expectedDistances) {
-		t.Errorf("got %d, wanted %d", distances, expectedDistances)
-	}
+	assert.Equal(t, expectedTimes, times)
+	assert.Equal(t, expectedDistances, distances)
 }
--- 2023/dayX/main_test.go
+++ 2023/dayX/main_test.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-var i = ``
+var i = `Time:      7  15   30
+Distance:  9  40  200`
 
 func TestTask1(t *testing.T) {
 	got := task1(i)
-	want := 0
+	want := 288
 
-	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
-	}
+	assert.Equal(t, got, want)
 }
 
 func TestTask2(t *testing.T) {
 
 	got := task2(i)
-	want := 0
-	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
-	}
+	want := 71503
+
+	assert.Equal(t, got, want)
 }

450d60af7aaf07b6a60e5a8b4a661b509c09c4ab
Damitha <damitha@canva.com>
2023-12-06 17:12:57 +1100

    day6

--- 2023/day6/main.go
+++ 2023/day6/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/morphkurt/adventofcode/util"
 )
@@ -14,11 +18,60 @@ func main() {
 	fmt.Printf("task2:%d\n", task2)
 }
 
-func task2(input string) int {
-	return 0
+func task1(input string) int {
+	result := 1
+	times, distances := parse(input)
+	for i, t := range times {
+		r := 0
+		d := distances[i]
+		for j := 0; j <= t; j++ {
+			if (t-j)*j > d {
+				r++
+			}
+		}
+		result = result * r
+	}
+	return result
 
 }
 
-func task1(input string) int {
-	return 0
+func task2(input string) int {
+	times, distances := parse(input)
+	concatTimes := ""
+	concatDistances := ""
+	for i, t := range times {
+		concatTimes += strconv.Itoa(t)
+		concatDistances += strconv.Itoa(distances[i])
+	}
+	t, _ := strconv.Atoi(concatTimes)
+	d, _ := strconv.Atoi(concatDistances)
+
+	r := 0
+	for j := 0; j <= t; j++ {
+		if (t-j)*j > d {
+			r++
+		}
+	}
+	return r
+
+}
+
+func parse(input string) ([]int, []int) {
+	time, distance := []int{}, []int{}
+	var re = regexp.MustCompile(`(?m)(\d)+`)
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	for scanner.Scan() {
+		line := scanner.Text()
+		ds := re.FindAllString(line, -1)
+		for _, d := range ds {
+			dv, _ := strconv.Atoi(d)
+			if strings.Contains(line, "Time") {
+				time = append(time, dv)
+			}
+			if strings.Contains(line, "Distance") {
+				distance = append(distance, dv)
+			}
+		}
+	}
+	return time, distance
 }
--- 2023/day6/main_test.go
+++ 2023/day6/main_test.go
@@ -4,11 +4,12 @@ import (
 	"testing"
 )
 
-var i = ``
+var i = `Time:      7  15   30
+Distance:  9  40  200`
 
 func TestTask1(t *testing.T) {
 	got := task1(i)
-	want := 0
+	want := 288
 
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
@@ -18,8 +19,24 @@ func TestTask1(t *testing.T) {
 func TestTask2(t *testing.T) {
 
 	got := task2(i)
-	want := 0
+	want := 71503
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func TestParsing(t *testing.T) {
+	times, distances := parse(i)
+	expectedTimes := []int{7, 15, 30}
+	expectedDistances := []int{9, 40, 200}
+	for i, et := range expectedTimes {
+		if times[i] != et {
+			t.Errorf("got %d, wanted %d", times[i], et)
+		}
+	}
+	for i, ed := range expectedDistances {
+		if distances[i] != ed {
+			t.Errorf("got %d, wanted %d", distances[i], ed)
+		}
 	}
 }

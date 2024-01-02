45359aad0cf40660ded8802eb37d8870831dde69
Damitha <damitha@canva.com>
2024-01-02 12:02:05 +1100

    day14

--- 2022/day14/main.go
+++ 2022/day14/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"sort"
+	"math"
 	"strings"
 
 	"github.com/morphkurt/adventofcode/util"
 )
+
+var D = []int{0, 1}
+var L = []int{-1, 0}
+var R = []int{1, 0}
+
+type Point struct {
+	x int
+	y int
+}
 
 func main() {
 	input := util.ReadFile("input")
@@ -18,67 +26,207 @@ func main() {
 }
 
 func task1(input string) int {
-	result := 0
-	packets := parse(input)
-	for i, p := range packets {
-		if (cmp(p[0], p[1])) <= 0 {
-			result += i + 1
+	p := parse(input)
+	m, _, _, minX, _ := toMapTask1(p)
+	c := 0
+	i := 1
+	for {
+		dropSand(m, Point{500 - minX, 0})
+		cc := count(m)
+		if cc > c {
+			c = cc
+		} else {
+			return i - 1
 		}
-
+		i++
 	}
-	return result
 }
 
 func task2(input string) int {
-	result := 1
-	packets := parse(input)
-	allPackets := []any{}
-	for _, p := range packets {
-		allPackets = append(allPackets, p...)
-	}
-	allPackets = append(allPackets, []any{[]any{2.}}, []any{[]any{6.}})
-	sort.Slice(allPackets, func(i, j int) bool { return cmp(allPackets[i], allPackets[j]) < 0 })
-	for i, p := range allPackets {
-		if fmt.Sprint(p) == "[[2]]" || fmt.Sprint(p) == "[[6]]" {
-			result *= i + 1
+	p := parse(input)
+	m, _, _, _, _ := toMapTask2(p)
+	c := 0
+	i := 0
+	for {
+		dropSand(m, Point{len(m[0])/2 + 1, 0})
+		cc := count(m)
+		if cc > c {
+			c = cc
+		} else {
+			return i
 		}
+		i++
 	}
-	return result
 }
 
-func parse(input string) [][]any {
-	out := [][]any{}
-	blocks := strings.Split(input, "\n\n")
-	for _, block := range blocks {
-		lines := strings.Split(block, "\n")
-		left := lines[0]
-		right := lines[1]
-		var leftItem any
-		var rightItem any
-		json.Unmarshal([]byte(left), &leftItem)
-		json.Unmarshal([]byte(right), &rightItem)
-		out = append(out, []any{leftItem, rightItem})
+func parse(input string) [][]Point {
+	out := [][]Point{}
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		lc := []Point{}
+		points := strings.Split(line, " -> ")
+		for _, p := range points {
+			c := util.ToInt(strings.Split(p, ","))
+			lc = append(lc, Point{c[0], c[1]})
+		}
+		out = append(out, lc)
 	}
 	return out
 }
 
-func cmp(a, b any) int {
-	as, aok := a.([]any)
-	bs, bok := b.([]any)
-
-	switch {
-	case !aok && !bok:
-		return int(a.(float64) - b.(float64))
-	case !aok:
-		as = []any{a}
-	case !bok:
-		bs = []any{b}
+func nextMove(m [][]rune, s Point) (Point, bool) {
+	moves := []Point{
+		{x: s.x + D[0], y: s.y + D[1]},               //down
+		{x: s.x + D[0] + L[0], y: s.y + D[1] + L[1]}, //down left
+		{x: s.x + D[0] + R[0], y: s.y + D[1] + R[1]}, //down right
 	}
-
-	for i := 0; i < len(as) && i < len(bs); i++ {
-		if c := cmp(as[i], bs[i]); c != 0 {
-			return c
+	for _, move := range moves {
+		if within(m, move) {
+			loc := m[move.y][move.x]
+			if loc == '.' {
+				return move, false
+			}
+		} else {
+			return s, true
 		}
 	}
-	return len(as) - len(bs)
+	return s, false
+}
+
+func within(m [][]rune, s Point) bool {
+	if s.y >= len(m) || s.y < 0 {
+		return false
+	}
+	if s.x >= len(m[0]) || s.x < 0 {
+		return false
+	}
+	return true
+}
+
+func dropSand(m [][]rune, s Point) {
+	settled := false
+	hitAbysis := false
+	next, curr := s, s
+	for {
+		next, hitAbysis = nextMove(m, curr)
+		if curr.x == next.x && curr.y == next.y {
+			settled = true
+		}
+		if hitAbysis {
+			break
+		}
+		if settled {
+			m[next.y][next.x] = 'o'
+			break
+		}
+		curr = next
+	}
+}
+
+func toMapTask1(p [][]Point) (out [][]rune, maxX, maxY, minX, minY int) {
+	out = [][]rune{}
+	maxX, maxY = math.MinInt, math.MinInt
+	minX, minY = math.MaxInt, 0
+	for _, lines := range p {
+		for _, point := range lines {
+			if point.x > maxX {
+				maxX = point.x
+			}
+			if point.y > maxY {
+				maxY = point.y
+			}
+			if point.x < minX {
+				minX = point.x
+			}
+		}
+	}
+	for y := 0; y <= maxY-minY; y++ {
+		row := []rune{}
+		for x := 0; x <= maxX-minX; x++ {
+			row = append(row, '.')
+		}
+		out = append(out, row)
+	}
+	for _, lines := range p {
+		for i := 0; i < len(lines)-1; i++ {
+			cur := lines[i]
+			next := lines[i+1]
+			fromX := math.Min(float64(cur.x), float64(next.x))
+			toX := math.Max(float64(cur.x), float64(next.x))
+			fromY := math.Min(float64(cur.y), float64(next.y))
+			toY := math.Max(float64(cur.y), float64(next.y))
+			for y := fromY; y <= toY; y++ {
+				for x := fromX; x <= toX; x++ {
+					out[int(y)-minY][int(x)-minX] = '#'
+				}
+			}
+		}
+	}
+	out[0][500-minX] = '+'
+	return out, maxX, maxY, minX, minY
+}
+
+func toMapTask2(p [][]Point) (out [][]rune, maxX, maxY, minX, minY int) {
+	out = [][]rune{}
+	maxX, maxY = math.MinInt, math.MinInt
+	minX, minY = math.MaxInt, 0
+	for _, lines := range p {
+		for _, point := range lines {
+			if point.x > maxX {
+				maxX = point.x
+			}
+			if point.y > maxY {
+				maxY = point.y
+			}
+			if point.x < minX {
+				minX = point.x
+			}
+		}
+	}
+	for y := 0; y <= (maxY-minY)+2; y++ {
+		row := []rune{}
+		for x := 0; x <= (maxY-minY+3)*2; x++ {
+			row = append(row, '.')
+		}
+		out = append(out, row)
+	}
+	for _, lines := range p {
+		for i := 0; i < len(lines)-1; i++ {
+			cur := lines[i]
+			next := lines[i+1]
+			offset := (len(out[0])/2 + 1) - (500 - minX)
+			fromX := math.Min(float64(cur.x), float64(next.x))
+			toX := math.Max(float64(cur.x), float64(next.x))
+			fromY := math.Min(float64(cur.y), float64(next.y))
+			toY := math.Max(float64(cur.y), float64(next.y))
+			for y := fromY; y <= toY; y++ {
+				for x := fromX; x <= toX; x++ {
+					out[int(y)-minY][int(x)-minX+offset] = '#'
+				}
+			}
+		}
+	}
+	for x := 0; x < len(out[0]); x++ {
+		out[len(out)-1][x] = '#'
+	}
+	out[0][len(out[0])/2+1] = '+'
+	return out, maxX, maxY, minX, minY
+}
+
+func printMatrix(m [][]rune) {
+	for _, line := range m {
+		fmt.Println(string(line))
+	}
+}
+
+func count(m [][]rune) int {
+	count := 0
+	for _, row := range m {
+		for _, c := range row {
+			if c == 'o' {
+				count++
+			}
+		}
+	}
+	return count
 }
--- 2022/day14/main_test.go
+++ 2022/day14/main_test.go
@@ -6,38 +6,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var taskOneInput = `[1,1,3,1,1]
-[1,1,5,1,1]
-
-[[1],[2,3,4]]
-[[1],4]
-
-[9]
-[[8,7,6]]
-
-[[4,4],4,4]
-[[4,4],4,4,4]
-
-[7,7,7,7]
-[7,7,7]
-
-[]
-[3]
-
-[[[]]]
-[[]]
-
-[1,[2,[3,[4,[5,6,7]]]],8,9]
-[1,[2,[3,[4,[5,6,0]]]],8,9]`
+var taskOneInput = `498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9`
 
 func TestTask1(t *testing.T) {
 	got := task1(taskOneInput)
-	want := 13
+	want := 24
 	assert.Equal(t, want, got)
 }
 
 func TestTask2(t *testing.T) {
 	got := task2(taskOneInput)
-	want := 140
+	want := 93
 	assert.Equal(t, want, got)
 }
